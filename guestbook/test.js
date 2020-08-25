d1cb8a825ac42f09379112fa23d44abbbe9806d9
root <root@daniel-918369.us-west1-b.c.abiding-operand-276609.internal>
2020-08-25 21:53:15 +0800

    node connect

--- guestbook/test.js
+++ guestbook/test.js
@@ -3,18 +3,25 @@ var http = require("http");
 var express = require("express");
 var app = express();
 
-var cors = require('cors')
+/*var cors = require('cors')
 
-app.use(cors()) // Use this after the variable declaration
+app.use(cors()) // Use this after the variable declaration*/
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 var con = mysql.createConnection(
     {
-        host: "localhost",
+        host: "34.105.17.84",
         user: "root",
-        password : "daniel918172",
+        password : "406261688",
         database: "my_db"
     }
-)
-
+);
+/*
+app.get('/api',function(req,res){
+	res.send("success");
+});
+*/
 app.post('/123asd', function(req, res)
 {
     var getData = 'select * from test1';
@@ -23,8 +30,8 @@ app.post('/123asd', function(req, res)
         if(err)throw err;
         res.send(result);
     })
-})
+});
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Guestbook app started on port 3000.");
   });
